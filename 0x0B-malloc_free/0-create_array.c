7351998a586e599d3f50b9b4169c955923d734ca
aymane-17 <aymane.elkhall@gmail.com>
2024-07-16 14:15:47 -0700

    first push

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - creates array of char, initialized with a specific char.
+ * @size: size of the array.
+ * @c: storaged car.
+ *
+ * Return: a pointer of	an array of chars
+ */
+char *creates_array(unsigned int size, char c) {
+	char *cr;
+	unsigned int i;
+
+	if(size == 0)
+		return(NULL);
+	cr = malloc(sizeof(c) * size);
+
+	if(cr == NULL)
+		return(NULL);
+
+	for(i = 0; i < size; i++)
+		cr[i] = c;
+
+	return(cr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifdef _MAIN_H_
+#define _MAIN_H_
+
+char *create_array(unsigned int size, char c);
+
+#endif
