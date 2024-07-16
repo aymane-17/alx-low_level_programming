87c20c50c2ef9212cfc72f069f0e11d105516816
aymane-17 <aymane.elkhall@gmail.com>
2024-07-16 14:24:11 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,14 @@ char *create_array(unsigned int size, char c)
 	char *cr;
 	unsigned int i;
 
-	if(size == 0)
+	if (size == 0)
 		return (NULL);
 	cr = malloc(sizeof(c) * size);
 
-	if(cr == NULL)
+	if (cr == NULL)
 		return (NULL);
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		cr[i] = c;
 
 	return (cr);
