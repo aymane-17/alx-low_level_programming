cacbb94b10ccb4f6662f2932c791e4027fe48cea
aymane-17 <aymane.elkhall@gmail.com>
2024-07-25 09:32:07 -0700

    hot fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,14 +16,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-	{	
+	{
 		strn = va_arg(ap, char *);
 
 		if (strn)
 			printf("%s", strn);
 		else
 			printf("(nil)");
-		if (i < n - 1)
+		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
 
